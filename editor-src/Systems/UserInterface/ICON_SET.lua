56e6e4a5f855a16d91b9563af55c44795d60252e
James <86011536+jammees@users.noreply.github.com>
2023-03-29 20:22:29 +0200

    Added icon set for toolbar handlers

---
+++ editor-src/Systems/UserInterface/ICON_SET.lua
@@ -0,0 +1,27 @@
+return {
+	ICON_SET_RESIZEPOINTS_RESIZE_DRAG = {
+		ICON_SET_ID = "rbxassetid://12905248305",
+		ICON_SET_MAP = {
+			[1] = Vector2.new(0, 0),
+			[2] = Vector2.new(256, 0),
+			[3] = Vector2.new(512, 0),
+			[4] = Vector2.new(0, 256),
+			[5] = Vector2.new(256, 256),
+			[6] = Vector2.new(512, 256),
+			[7] = Vector2.new(0, 512),
+			[8] = Vector2.new(256, 512),
+		},
+	},
+
+	ICON_SET_OBJ_ARROW = {
+		ICON_SET_ID = "rbxassetid://12704209971",
+		ICON_SET_MAP = {
+			camera = Vector2.new(0, 0),
+			warning_icon = Vector2.new(44, 0),
+			checkmark = Vector2.new(88, 0),
+			folder = Vector2.new(88 + 44, 0),
+			prot_rigidbody_obj = Vector2.new(88 + 88, 0),
+			prot_uibase_obj = Vector2.new(44, 44),
+		},
+	},
+}
