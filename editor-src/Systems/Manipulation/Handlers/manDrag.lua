81135d2820f46807a483f88152d81be08d4d05f3
James <86011536+jammees@users.noreply.github.com>
2023-03-29 20:20:53 +0200

    Added drag functionality

--- editor-src/Systems/Manipulation/Handlers/manDrag.lua
+++ editor-src/Systems/Manipulation/Handlers/manDrag.lua
@@ -1,3 +1,5 @@
+local GRID_SIZE = 10
+
 local library = script.Parent.Parent.Parent.Parent.Library
 local systems = script.Parent.Parent.Parent
 local pivots = script.Parent.Parent.Pivots
@@ -5,19 +7,28 @@ local pivots = script.Parent.Parent.Pivots
 local MouseSystem = require(systems.Mouse)
 local ObjectSystem = require(systems.Object)
 local UserInterfaceSystem = require(systems.UserInterface)
+local ConfigSystem = require(systems.Config)
 
 local Janitor = require(library.Janitor).new()
+
+local function Snap(Input)
+	if ConfigSystem.man_SnapToGrid:get() == false then
+		return Input
+	end
+
+	return math.round(Input / GRID_SIZE) * GRID_SIZE
+end
 
 local function man_DragXY(reference: any, dragX: boolean, dragY: boolean)
 	local object = reference.EditorData
 	local mouseDelta = MouseSystem.GetDelta()
 
-	if dragX then
-		object.man_fPosition = Vector2.new(object.man_OGPosition.X + mouseDelta.X, object.man_fPosition.Y)
+	if dragX == 1 then
+		object.man_fPosition = Vector2.new(Snap(object.man_OGPosition.X + mouseDelta.X), object.man_fPosition.Y)
 	end
 
-	if dragY then
-		object.man_fPosition = Vector2.new(object.man_fPosition.X, object.man_OGPosition.Y + mouseDelta.Y)
+	if dragY == 1 then
+		object.man_fPosition = Vector2.new(object.man_fPosition.X, Snap(object.man_OGPosition.Y + mouseDelta.Y))
 	end
 end
 
@@ -25,25 +36,44 @@ local Handler = {}
 
 function Handler.Mount(object: any)
 	local reference = ObjectSystem.GetFromObject(object)
-	local pivotClass = require(pivots.manDrag.draggerBase).new(object, 1)
+	local pivotClass = require(pivots.manDrag.draggerBase).new(object)
+
+	Janitor:Add(pivotClass)
 
 	local isDragging = false
-	local resizeDir = { 0, 0 }
+	local dragDirection = { 0, 0 }
+
+	Janitor:Add(pivotClass.DragStart:Connect(function(dragDir)
+		dragDirection = dragDir
+		isDragging = true
+
+		UserInterfaceSystem.UI.Detector.ZIndex = 25
+	end))
+
+	Janitor:Add(pivotClass.DragEnd:Connect(function()
+		isDragging = false
+		dragDirection = nil
+
+		UserInterfaceSystem.UI.Detector.ZIndex = 1
+	end))
 
 	Janitor:Add(UserInterfaceSystem.UI.Detector.MouseButton1Up:Connect(function()
 		isDragging = false
+		dragDirection = nil
 
 		UserInterfaceSystem.UI.Detector.ZIndex = 1
 	end))
 
 	Janitor:Add(MouseSystem.Moved:Connect(function()
 		if isDragging == true then
-			--man_DragXY(reference, table.unpack(resizeDir))
+			man_DragXY(reference, table.unpack(dragDirection))
 
 			object.Position =
 				UDim2.fromOffset(reference.EditorData.man_fPosition.X, reference.EditorData.man_fPosition.Y - 130)
 
 			reference.EditorData.man_OGPosition = object.AbsolutePosition
+
+			pivotClass:UpdatePos()
 		end
 	end))
 end
