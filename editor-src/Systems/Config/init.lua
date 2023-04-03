733c687e7e73f7d69a3f275ffec45e1850f686f1
James <86011536+jammees@users.noreply.github.com>
2023-04-03 20:38:00 +0200

    Added more settings

--- editor-src/Systems/Config/init.lua
+++ editor-src/Systems/Config/init.lua
@@ -3,4 +3,7 @@ local Value = require(script.Value)
 return {
 	man_Mode = Value.new(0),
 	man_SnapToGrid = Value.new(false),
+	man_GridSize = Value.new(20),
+	ui_TopbarOffset = Value.new(130),
+	ui_PropertySize = Value.new(250),
 }
