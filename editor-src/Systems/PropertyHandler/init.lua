ec345a271383d4767611388fd56b00ead95d764c
James <86011536+jammees@users.noreply.github.com>
2023-04-05 23:49:39 +0200

    Added TODO and property handlers

--- editor-src/Systems/PropertyHandler/init.lua
+++ editor-src/Systems/PropertyHandler/init.lua
@@ -1,13 +1,77 @@
+type Property = {
+	MemberType: "Property",
+	Category: string,
+	Name: string,
+	Security: {
+		Read: string,
+		Write: string,
+	},
+	Serialization: {
+		CanLoad: boolean,
+		CanSave: boolean,
+	},
+	Tags: { string }?,
+	ThreadSafety: string,
+	ValueType: {
+		Category: string,
+		Name: string,
+	},
+}
+
+--[[
+	TODO: List of all of the handlers needed
+
+	[x] Class selection
+	[x] int
+	[x] float
+	[x] bool
+	[ ] SizeConstraint
+	[ ] GuiObject
+	[x] Color3
+	[ ] BorderMode
+	[ ] Vector2
+	[ ] string
+	[ ] AutomaticSize
+	[ ] UDim2
+	[ ] SelectionBehavior
+	[ ] LocalizationTable
+	[ ] Rect
+	[ ] ScaleType
+	[ ] Content
+	[ ] ResamplerMode
+	[ ] ButtonStyle
+	[ ] Camera
+	[ ] Vector3
+	[ ] TextTruncate
+	[ ] TextYAlignment
+	[ ] Font
+	[ ] TextXAlignment
+	[ ] FrameStyle
+	[ ] ScrollingDirection
+	[ ] ScrollBarInset
+	[ ] ElasticBehavior
+	[ ] VerticalScrollBarPosition
+
+]]
+
 local library = script.Parent.Parent.Library
 
 local SelectorSystem = require(script.Parent.Selector)
 local UserInterfaceSystem = require(script.Parent.UserInterface)
+local ObjectSystem = require(script.Parent.Object)
 
 local Janitor = require(library.Janitor).new()
-local DefaultProperties = require(script.Parent.Object.DefaultProperties)
+local DumpParser = require(library["dump-parser-0.1.1"])
+local Util = require(script.Util)
+
+local serverDump = DumpParser.fetchFromServer()
+
+local H_propertyBase = require(UserInterfaceSystem.Handlers.propertyBase)
 
 local handlers = {
-	H_createPropertyCheckbox = require(UserInterfaceSystem.Handlers.createPropertyCheckbox),
+	H_checkbox = require(UserInterfaceSystem.Handlers.checkbox),
+	H_numberField = require(UserInterfaceSystem.Handlers.numberField),
+	H_colorField = require(UserInterfaceSystem.Handlers.colorField),
 }
 
 local propertyWindow = nil
@@ -23,39 +87,65 @@ function PropertyHandler.Clear()
 end
 
 function PropertyHandler.GetHandlerDef(value: string)
-	if typeof(value) == "boolean" then
-		return "H_createPropertyCheckbox"
+	if value == "bool" then
+		return "H_checkbox"
+	elseif value == "int" or value == "float" then
+		return "H_numberField"
+	elseif value == "Color3" then
+		return "H_colorField"
+	elseif value == "Instance" then
+		return nil
+	elseif value == "Vector2" then
+		return nil
+	elseif value == "UDim2" then
+		return nil
 	end
+
 	return nil
 end
 
 function PropertyHandler.LoadFrom<OBJ>(object: OBJ)
+	local objectProperties = serverDump:GetProperties(object, DumpParser.Filter.Invert(DumpParser.Filter.ReadOnly))
+
 	-- do stuffs
-	for propName, defaultValue in DefaultProperties[object.ClassName] do
-		local handlerDef = PropertyHandler.GetHandlerDef(defaultValue)
+	for propName, data: Property in objectProperties do
+		local handlerDef = PropertyHandler.GetHandlerDef(data.ValueType.Name)
 
 		if not handlerDef then
 			continue
 		end
 
-		local handler = handlers[handlerDef]({
+		local base = H_propertyBase({
 			Property = propName,
-			Object = object,
-			InitialState = object[propName],
+			Handler = handlers[handlerDef]({
+				--Title = propName,
+				Object = object,
+				InitialState = object[propName],
+				InitialValue = object[propName],
+				OnStateChange = function(newValue: any)
+					object[propName] = newValue
+					ObjectSystem.GetFromObject(object).ExportData.Properties[propName] = newValue
+				end,
+				Range = Util.GetRangeFromProperty(propName),
+			}),
 		})
 
-		handler.Parent = propertyWindow.Contents
+		base.Parent = propertyWindow.Contents
 
-		table.insert(loadedHandlers, handler)
+		table.insert(loadedHandlers, base)
 	end
 end
 
 function PropertyHandler.Start()
 	propertyWindow = UserInterfaceSystem.UI.Editor.Property
 
-	Janitor:Add(SelectorSystem.SelectionChanged:Connect(function()
+	Janitor:Add(SelectorSystem.Triggered:Connect(function()
 		PropertyHandler.Clear()
 		PropertyHandler.LoadFrom(SelectorSystem.Selected)
+	end))
+
+	Janitor:Add(UserInterfaceSystem.UI.Detector.MouseButton1Click:Connect(function()
+		PropertyHandler.Clear()
 	end))
 end
 
