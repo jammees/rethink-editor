e717dc241ac830c9337287af3d5b560f26192793
James <86011536+jammees@users.noreply.github.com>
2023-04-05 23:53:34 +0200

    Added debug buttons
    Export default properties and get ExportData

--- editor-src/Systems/UserInterface/Toolbar.lua
+++ editor-src/Systems/UserInterface/Toolbar.lua
@@ -3,11 +3,11 @@ local handlers = script.Parent.Handlers
 
 local ConfigSystem = require(script.Parent.Parent.Config)
 
-local H_createToolbarButtonBig = require(handlers.createToolbarButtonBig)
-local H_createToolbarCategory = require(handlers.createToolbarCategory)
-local H_createToolbarCheckbox = require(handlers.createToolbarCheckbox)
-local H_toolbarCheckboxController = require(handlers.toolbarCheckboxController)
-local H_createToolbarNumberField = require(handlers.createToolbarNumberField)
+local H_toolbarButton = require(handlers.toolbarButton)
+local H_toolbarCategory = require(handlers.toolbarCategory)
+local H_checkbox = require(handlers.checkbox)
+local H_unifier = require(handlers.unifier)
+local H_numberField = require(handlers.numberField)
 
 local Fusion = require(library.Fusion)
 local Children = Fusion.Children
@@ -36,12 +36,12 @@ return function()
 				PaddingBottom = UDim.new(0, 5),
 			}),
 
-			H_createToolbarCategory({
+			H_toolbarCategory({
 				Separator = "right",
 				Title = "Object Manipulation",
 
 				Handlers = {
-					H_createToolbarButtonBig({
+					H_toolbarButton({
 						Title = "Drag",
 						IconIDX = 8,
 						Priority = 1,
@@ -54,7 +54,7 @@ return function()
 						end,
 					}),
 
-					H_createToolbarButtonBig({
+					H_toolbarButton({
 						Title = "Resize",
 						IconIDX = 7,
 						Priority = 2,
@@ -67,13 +67,13 @@ return function()
 						end,
 					}),
 
-					H_createToolbarCategory({
+					H_toolbarCategory({
 						Direction = Enum.FillDirection.Vertical,
 						Priority = 3,
 						Handlers = {
-							H_toolbarCheckboxController({
+							H_unifier({
 								Checkboxes = {
-									H_createToolbarCheckbox({
+									H_checkbox({
 										Title = "Snap to Grid",
 										InitialState = false,
 										OnStateChange = function(newState: boolean)
@@ -81,7 +81,7 @@ return function()
 										end,
 									}),
 
-									H_createToolbarNumberField({
+									H_numberField({
 										InitialValue = ConfigSystem.man_GridSize:get(),
 										OnValueChange = function(newValue)
 											ConfigSystem.man_GridSize:set(newValue)
@@ -91,12 +91,132 @@ return function()
 
 								Modifiers = {
 									unifiedXSize = true,
-									--relativeSize = true,
 								},
 							}),
 						},
 					}),
 				},
+			}),
+
+			H_toolbarButton({
+				Title = "Get export data from",
+				OnClick = function()
+					local ObjectSys = require(script.Parent.Parent.Object)
+					local SelectorSys = require(script.Parent.Parent.Selector)
+
+					print(ObjectSys.GetFromObject(SelectorSys.Selected).ExportData)
+				end,
+			}),
+
+			-- Oh no
+			-- bad code
+			-- very bad code
+			-- that works somehow
+			-- dont ask me how
+			H_toolbarButton({
+				Title = "Export default properties",
+				OnClick = function()
+					local DumpParser = require(library["dump-parser-0.1.1"])
+					local dump = DumpParser.fetchFromServer()
+					local filter = dump.Filter
+
+					local objectsToScan: Folder = game:GetService("Lighting").ObjectsToScan
+
+					local exportedData = {}
+					local handlerTypes = {}
+
+					for _, obj in objectsToScan:GetChildren() do
+						local props = dump:GetProperties(obj, filter.Invert(filter.ReadOnly))
+
+						exportedData[obj.ClassName] = props
+					end
+
+					print(exportedData)
+
+					local module = Instance.new("ModuleScript")
+					module.Name = "Default Properties"
+					module.Parent = game.Lighting
+
+					local moduleSource = "--Exported default UI properties using DumpParser\n\nreturn {"
+
+					for class, props in exportedData do
+						moduleSource = moduleSource .. ("\n\t%s = {"):format(class)
+
+						for
+							propName,
+							data: {
+							MemberType: "Property",
+							Category: string,
+							Name: string,
+							Security: {
+								Read: string,
+								Write: string,
+							},
+							Serialization: {
+								CanLoad: boolean,
+								CanSave: boolean,
+							},
+							Tags: { string }?,
+							ThreadSafety: string,
+							ValueType: {
+								Category: string,
+								Name: string,
+							},
+						}
+						in props do
+							if propName == "Parent" then
+								continue
+							end
+
+							if table.find(handlerTypes, data.ValueType.Name) == nil then
+								table.insert(handlerTypes, data.ValueType.Name)
+							end
+
+							local objProperty = objectsToScan:FindFirstChildOfClass(class)[propName]
+							local serialized = nil
+
+							if typeof(objProperty) == "string" then
+								serialized = `\"{objProperty}\"`
+							elseif typeof(objProperty) == "UDim2" then
+								local udim2: UDim2 = objProperty
+								serialized =
+									`UDim2.new({udim2.X.Scale}, {udim2.X.Offset}, {udim2.Y.Scale}, {udim2.Y.Offset})`
+							elseif typeof(objProperty) == "Color3" then
+								serialized = `Color3.new({objProperty})`
+							elseif typeof(objProperty) == "number" or typeof(objProperty) == "boolean" then
+								serialized = tostring(objProperty)
+							elseif typeof(objProperty) == "Vector2" then
+								serialized = `Vector2.new({objProperty})`
+							elseif typeof(objProperty) == "Font" then
+								local font: Font = objProperty
+								serialized = `Font.new("{font.Family}", {font.Weight}, {font.Style})`
+							elseif typeof(objProperty) == "EnumItem" then
+								serialized = tostring(objProperty)
+							elseif typeof(objProperty) == "nil" then
+								serialized = "nil"
+							elseif typeof(objProperty) == "Rect" then
+								local rect: Rect = objProperty
+								serialized =
+									`Rect.new(Vector2.new({rect.Min.X}, {rect.Min.Y}), Vector2.new({rect.Max.X}, {rect.Max.Y}))`
+							elseif typeof(objProperty) == "Vector3" then
+								local vec3: Vector3 = objProperty
+								serialized = `Vector3.new({vec3.X}, {vec3.Y}, {vec3.Z})`
+							else
+								warn(`{propName} = {typeof(objProperty)} for {class} not processed!`)
+							end
+
+							moduleSource = moduleSource .. ("\n\t\t%s = %s,"):format(propName, serialized)
+						end
+
+						moduleSource = moduleSource .. "\n\t},"
+					end
+
+					moduleSource = moduleSource .. "\n}"
+
+					module.Source = moduleSource
+
+					print(handlerTypes)
+				end,
 			}),
 		},
 	})
