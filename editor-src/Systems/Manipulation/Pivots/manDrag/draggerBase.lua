c20c4b073e90ab07478baee7af73581225bc68f5
James <86011536+jammees@users.noreply.github.com>
2023-04-29 12:02:37 +0200

    UI changes

--- editor-src/Systems/Manipulation/Pivots/manDrag/draggerBase.lua
+++ editor-src/Systems/Manipulation/Pivots/manDrag/draggerBase.lua
@@ -8,6 +8,7 @@ local library = script.Parent.Parent.Parent.Parent.Parent.Library
 
 local UserInterfaceSystem = require(script.Parent.Parent.Parent.Parent.UserInterface)
 
+local ICON_SET = require(script.Parent.Parent.Parent.Parent.UserInterface.ICON_SET)
 local Signal = require(library.Signal)
 local Janitor = require(library.Janitor)
 local Fusion = require(library.Fusion)
@@ -45,86 +46,77 @@ function DraggerBase.new(object: GuiBase2d)
 		[OnEvent("MouseButton1Up")] = function()
 			self.DragEnd:Fire()
 		end,
-	})
 
-	self.YDragger = New("ImageButton")({
-		Image = ICON_SET_ID,
-		ImageRectSize = Vector2.new(42, 176),
-		ImageRectOffset = Vector2.new(0, 44),
-		BackgroundTransparency = 1,
-		ImageColor3 = Color3.fromHex("54CF49"),
-		Parent = UserInterfaceSystem.UI,
-		ZIndex = 99999991,
-		Name = "YDragger",
-		AnchorPoint = Vector2.new(0.5, 1),
-		Size = UDim2.fromOffset(35, 150),
-		Position = UDim2.fromOffset(
-			object.AbsolutePosition.X + object.AbsoluteSize.X / 2,
-			object.AbsolutePosition.Y + object.AbsoluteSize.Y / 2 - 5.25
-		),
+		[Fusion.Children] = {
+			YDragger = New("ImageButton")({
+				Image = ICON_SET.drag_arrow,
+				BackgroundTransparency = 1,
+				ImageColor3 = Color3.fromHex("54CF49"),
+				Parent = UserInterfaceSystem.UI,
+				ZIndex = 99999991,
+				Name = "YDragger",
+				AnchorPoint = Vector2.new(0.5, 1),
+				Size = UDim2.fromOffset(35, 150),
+				Position = UDim2.fromScale(0.5, 0),
 
-		[OnEvent("MouseButton1Down")] = function()
-			self.DragStart:Fire({ 0, 1 })
-		end,
+				[OnEvent("MouseButton1Down")] = function()
+					self.DragStart:Fire({ 0, 1 })
+				end,
 
-		[OnEvent("MouseButton1Up")] = function()
-			self.DragEnd:Fire()
-		end,
+				[OnEvent("MouseButton1Up")] = function()
+					self.DragEnd:Fire()
+				end,
 
-		[OnEvent("MouseEnter")] = function()
-			--self.YDragger.ImageColor3 = Color3.fromRGB(47, 136, 39)
-			self.YDragger.ImageColor3 = Color3.fromRGB(
-				84 - DARKENING_SELECTED_AMOUNT_THINGY,
-				207 - DARKENING_SELECTED_AMOUNT_THINGY,
-				73 - DARKENING_SELECTED_AMOUNT_THINGY
-			)
-		end,
+				[OnEvent("MouseEnter")] = function()
+					--self.YDragger.ImageColor3 = Color3.fromRGB(47, 136, 39)
+					self.XYDragger.YDragger.ImageColor3 = Color3.fromRGB(
+						84 - DARKENING_SELECTED_AMOUNT_THINGY,
+						207 - DARKENING_SELECTED_AMOUNT_THINGY,
+						73 - DARKENING_SELECTED_AMOUNT_THINGY
+					)
+				end,
 
-		[OnEvent("MouseLeave")] = function()
-			self.YDragger.ImageColor3 = Color3.fromRGB(84, 207, 73)
-		end,
-	})
+				[OnEvent("MouseLeave")] = function()
+					self.XYDragger.YDragger.ImageColor3 = Color3.fromRGB(84, 207, 73)
+				end,
+			}),
 
-	self.XDragger = New("ImageButton")({
-		Image = ICON_SET_ID,
-		ImageRectSize = Vector2.new(176, 42),
-		ImageRectOffset = Vector2.new(44, 176),
-		BackgroundTransparency = 1,
-		ImageColor3 = Color3.fromRGB(207, 105, 73),
-		Parent = UserInterfaceSystem.UI,
-		ZIndex = 99999991,
-		Name = "YDragger",
-		AnchorPoint = Vector2.new(1, 0.5),
-		Size = UDim2.fromOffset(150, 35),
-		Position = UDim2.fromOffset(
-			object.AbsolutePosition.X + object.AbsoluteSize.X / 2 + 150 + 5.25,
-			object.AbsolutePosition.Y + object.AbsoluteSize.Y / 2
-		),
+			XDragger = New("ImageButton")({
+				Image = ICON_SET.drag_arrow,
+				Rotation = 90,
+				BackgroundTransparency = 1,
+				ImageColor3 = Color3.fromRGB(207, 105, 73),
+				Parent = UserInterfaceSystem.UI,
+				ZIndex = 99999991,
+				Name = "XDragger",
+				AnchorPoint = Vector2.new(0.5, 1),
+				Size = UDim2.fromOffset(35, 150),
+				Position = UDim2.new(0, 90, 0.5, 75),
 
-		[OnEvent("MouseButton1Down")] = function()
-			self.DragStart:Fire({ 1, 0 })
-		end,
+				[OnEvent("MouseButton1Down")] = function()
+					self.DragStart:Fire({ 1, 0 })
+				end,
 
-		[OnEvent("MouseButton1Up")] = function()
-			self.DragEnd:Fire()
-		end,
+				[OnEvent("MouseButton1Up")] = function()
+					self.DragEnd:Fire()
+				end,
 
-		[OnEvent("MouseEnter")] = function()
-			self.XDragger.ImageColor3 = Color3.fromRGB(
-				207 - DARKENING_SELECTED_AMOUNT_THINGY,
-				105 - DARKENING_SELECTED_AMOUNT_THINGY,
-				73 - DARKENING_SELECTED_AMOUNT_THINGY
-			)
-		end,
+				[OnEvent("MouseEnter")] = function()
+					self.XYDragger.XDragger.ImageColor3 = Color3.fromRGB(
+						207 - DARKENING_SELECTED_AMOUNT_THINGY,
+						105 - DARKENING_SELECTED_AMOUNT_THINGY,
+						73 - DARKENING_SELECTED_AMOUNT_THINGY
+					)
+				end,
 
-		[OnEvent("MouseLeave")] = function()
-			self.XDragger.ImageColor3 = Color3.fromRGB(207, 105, 73)
-		end,
+				[OnEvent("MouseLeave")] = function()
+					self.XYDragger.XDragger.ImageColor3 = Color3.fromRGB(207, 105, 73)
+				end,
+			}),
+		},
 	})
 
 	self.Janitor:Add(self.XYDragger)
-	self.Janitor:Add(self.YDragger)
-	self.Janitor:Add(self.XDragger)
 
 	return self
 end
@@ -133,16 +125,6 @@ function DraggerBase:UpdatePos()
 	self.XYDragger.Position = UDim2.fromOffset(
 		self.Object.AbsolutePosition.X + self.Object.AbsoluteSize.X / 2 - 5.25,
 		self.Object.AbsolutePosition.Y + self.Object.AbsoluteSize.Y / 2 - 5.25
-	)
-
-	self.YDragger.Position = UDim2.fromOffset(
-		self.Object.AbsolutePosition.X + self.Object.AbsoluteSize.X / 2,
-		self.Object.AbsolutePosition.Y + self.Object.AbsoluteSize.Y / 2 - 5.25
-	)
-
-	self.XDragger.Position = UDim2.fromOffset(
-		self.Object.AbsolutePosition.X + self.Object.AbsoluteSize.X / 2 + 150 + 5.25,
-		self.Object.AbsolutePosition.Y + self.Object.AbsoluteSize.Y / 2
 	)
 end
 
