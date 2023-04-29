a70ec78d583b55d6a5fe7af1106b24be6b67f888
James <86011536+jammees@users.noreply.github.com>
2023-04-29 12:05:41 +0200

    Changed icons

--- editor-src/Systems/UserInterface/ICON_SET.lua
+++ editor-src/Systems/UserInterface/ICON_SET.lua
@@ -1,37 +1,15 @@
 return {
-	ICON_SET_RESIZEPOINTS_RESIZE_DRAG = {
-		ICON_SET_ID = "rbxassetid://12905248305",
-		ICON_SET_MAP = {
-			[1] = Vector2.new(0, 0),
-			[2] = Vector2.new(256, 0),
-			[3] = Vector2.new(512, 0),
-			[4] = Vector2.new(0, 256),
-			[5] = Vector2.new(256, 256),
-			[6] = Vector2.new(512, 256),
-			[7] = Vector2.new(0, 512),
-			[8] = Vector2.new(256, 512),
-		},
-	},
-
-	ICON_SET_OBJ_ARROW = {
-		ICON_SET_ID = "rbxassetid://12704209971",
-		ICON_SET_MAP = {
-			camera = Vector2.new(0, 0),
-			warning_icon = Vector2.new(44, 0),
-			checkmark = Vector2.new(88, 0),
-			folder = Vector2.new(88 + 44, 0),
-			prot_rigidbody_obj = Vector2.new(88 + 88, 0),
-			prot_uibase_obj = Vector2.new(44, 44),
-		},
-	},
-
-	-- Grabbed from Interface Tools
-	ICON_SET_ARROW = {
-		ICON_SET_ID = "rbxassetid://3926307971",
-		ICON_SET_SIZE = Vector2.new(36, 36),
-		ICON_SET_MAP = {
-			down = Vector2.new(324, 524),
-			up = Vector2.new(164, 484),
-		},
-	},
+	anchor = "rbxassetid://13073306879",
+	checkmark = "rbxassetid://13073306609",
+	down_arrow_full = "rbxassetid://13073306343",
+	down_arrow_thin = "rbxassetid://13073306144",
+	drag_arrow = "rbxassetid://13073305875",
+	move = "rbxassetid://13073305555",
+	resize = "rbxassetid://13073305322",
+	resize_corner = "rbxassetid://13073305109",
+	resize_side = "rbxassetid://13073304804",
+	up_arrow_full = "rbxassetid://13073304452",
+	up_arrow_thin = "rbxassetid://13073304224",
+	editor_button_default = "rbxassetid://13141195759",
+	editor_button_download = "rbxassetid://13141195255",
 }
