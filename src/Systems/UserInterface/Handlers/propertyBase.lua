71f3b2dbc99cfa43cfc6ab324f00e8b0fc2a26e1
James <86011536+jammees@users.noreply.github.com>
2023-07-26 16:14:33 +0200

    Hover feedback, read size from config

--- src/Systems/UserInterface/Handlers/propertyBase.lua
+++ src/Systems/UserInterface/Handlers/propertyBase.lua
@@ -13,18 +13,26 @@ type Props = {
 
 local library = script.Parent.Parent.Parent.Parent.Library
 
+local ConfigSystem = require(script.Parent.Parent.Parent.Config).Get()
+
+local Themes = require(script.Parent.Parent.Themes).GetTheme()
+
 local Fusion = require(library.Fusion)
 local Children = Fusion.Children
 local New = Fusion.New
+local Ref = Fusion.Ref
+local Value = Fusion.Value
 
 return function(props: Props): Frame
+	local titleRef = Value()
+
 	-- Edit the handler
 	props.Handler:Get().Size = UDim2.new(1, 0, props.Handler:Get().Size.Y.Scale, props.Handler:Get().Size.Y.Offset)
 
 	return New("Frame")({
 		Size = UDim2.new(1, 0, 0, 25),
-		Name = props.Property .. " Listener",
-		BackgroundColor3 = Color3.fromRGB(32, 32, 32),
+		Name = props.Property,
+		BackgroundColor3 = Themes.BG1,
 		BorderSizePixel = 0,
 		LayoutOrder = props.Priority and props.Priority or 0,
 
@@ -38,7 +46,7 @@ return function(props: Props): Frame
 
 			TitleContainer = New("Frame")({
 				BackgroundTransparency = 1,
-				Size = UDim2.fromScale(0.5, 1),
+				Size = UDim2.fromScale(ConfigSystem.ui_PropertyBase_TitleWidht:get(), 1),
 				Name = "TitleContainer",
 
 				[Children] = {
@@ -46,21 +54,47 @@ return function(props: Props): Frame
 						Text = props.Property,
 						Size = UDim2.fromScale(1, 1),
 						BackgroundTransparency = 1,
-						TextColor3 = Color3.fromRGB(255, 255, 255),
+						TextColor3 = Themes.Text2,
 						TextXAlignment = Enum.TextXAlignment.Left,
 						TextTruncate = Enum.TextTruncate.AtEnd,
+
+						[Ref] = titleRef,
 					}),
 				},
 			}),
 
 			HandlerContainer = New("Frame")({
 				BackgroundTransparency = 1,
-				Size = UDim2.fromScale(0.5, 1),
-				Position = UDim2.fromScale(0.5, 0),
+				Size = UDim2.fromScale(ConfigSystem.ui_PropertyBase_HandlerWidht:get(), 1),
+				Position = UDim2.fromScale(ConfigSystem.ui_PropertyBase_TitleWidht:get(), 0),
 				Name = "HandlerContainer",
 
 				[Children] = { props.Handler:Get() },
 			}),
 		},
+
+		[Fusion.OnEvent("MouseEnter")] = function()
+			Fusion.peek(titleRef).TextColor3 = Themes.Text1
+
+			if props.Handler.InputRef then
+				Fusion.peek(props.Handler.BaseRef).BackgroundColor3 = Themes.BG3
+			elseif props.Handler.Handlers then
+				for _, v in props.Handler.Handlers do
+					Fusion.peek(v.BaseRef).BackgroundColor3 = Themes.BG3
+				end
+			end
+		end,
+
+		[Fusion.OnEvent("MouseLeave")] = function()
+			Fusion.peek(titleRef).TextColor3 = Themes.Text2
+
+			if props.Handler.InputRef then
+				Fusion.peek(props.Handler.BaseRef).BackgroundColor3 = Themes.BG2
+			elseif props.Handler.Handlers then
+				for _, v in props.Handler.Handlers do
+					Fusion.peek(v.BaseRef).BackgroundColor3 = Themes.BG2
+				end
+			end
+		end,
 	})
 end
