ee43e19a37740f310d2c182487a48e7af098f747
James <86011536+jammees@users.noreply.github.com>
2023-07-26 16:11:50 +0200

    Improvements to `ColorField` handler
    - Auto update if property changes
    - MuteChangedSignal
    - Use Theme

--- src/Systems/UserInterface/Handlers/Property/ColorField.lua
+++ src/Systems/UserInterface/Handlers/Property/ColorField.lua
@@ -1,3 +1,4 @@
+local DataStoreService = game:GetService("DataStoreService")
 --[[
 	colorField.lua
 
@@ -15,25 +16,27 @@ type Props = {
 
 local library = script.Parent.Parent.Parent.Parent.Parent.Library
 
+local ConfigSystem = require(script.Parent.Parent.Parent.Parent.Config).Get()
+
 local NumberField = require(script.Parent.NumberField)
+local Unifier = require(script.Parent.Parent.Unifier)
 
 local Fusion = require(library.Fusion)
 local Children = Fusion.Children
 local New = Fusion.New
-local Ref = Fusion.Ref
-local Value = Fusion.Value
 
 local ColorField = {}
 ColorField.__index = ColorField
 
-function ColorField.new(props)
-	print("new ColorField class")
-
+function ColorField.new(props: Props)
 	local self = setmetatable({}, ColorField)
+
+	self.Kind = "ColorField"
 
 	self.Props = props
 	self.State = { 255, 255, 255 }
 	self.Handlers = {}
+	self.MuteChangedSignal = false
 
 	-- Convert the number into a range of 0-255 instead of 0-1
 	-- Reason is it would be harder to make NumberField display the 0-255
@@ -44,6 +47,27 @@ function ColorField.new(props)
 	end
 
 	self.UI = self:Render()
+
+	if props.Object then
+		self:CleanupIf(
+			props.Janitor,
+			props.Object:GetPropertyChangedSignal(props.Property):Connect(function()
+				if self.MuteChangedSignal then
+					require(script.Parent.Parent.Parent.Parent.LoggerV1).Log(
+						"ColorField",
+						2,
+						`:GetPropertyChangedSignal muted!`
+					)
+					return
+				end
+
+				local newValue: Color3 = props.Object[props.Property]
+				local newState = { newValue.R, newValue.G, newValue.B }
+
+				self:SetState(newState)
+			end)
+		)
+	end
 
 	self:CleanupIf(props.Janitor, self.UI)
 
@@ -60,11 +84,11 @@ function ColorField:CleanupIf(statement: boolean, connection: RBXScriptSignal | 
 end
 
 function ColorField:SetState(newState: { number })
+	self.MuteChangedSignal = true
+
 	self.State = newState
 
-	print(self.Handlers)
-
-	for i = 1, 3 do
+	for i = 1, #self.Handlers do
 		local handlerClass = self.Handlers[i]
 
 		if not handlerClass then
@@ -74,14 +98,16 @@ function ColorField:SetState(newState: { number })
 		handlerClass:SetState(self.State[i], true)
 	end
 
-	self.Props.OnValueChange(Color3.new(table.unpack(self.State)))
+	self.Props.OnValueChange(Color3.fromRGB(table.unpack(self.State)))
+
+	self.MuteChangedSignal = false
 
 	return self
 end
 
 function ColorField:Render(): GuiBase2d
 	local container = New("Frame")({
-		Size = UDim2.new(0, 100, 0, 20),
+		Size = UDim2.fromScale(1, 1),
 		BackgroundTransparency = 1,
 		Name = "ColorField",
 		LayoutOrder = self.Props.Priority and self.Props.Priority or 0,
@@ -90,7 +116,7 @@ function ColorField:Render(): GuiBase2d
 			List = New("UIListLayout")({
 				FillDirection = Enum.FillDirection.Horizontal,
 				SortOrder = Enum.SortOrder.LayoutOrder,
-				Padding = UDim.new(0, 5),
+				Padding = UDim.new(0, ConfigSystem.ui_Property_Handler_Padding_Spacer:get()),
 			}),
 		},
 	})
@@ -99,7 +125,9 @@ function ColorField:Render(): GuiBase2d
 		local handler = NumberField.new({
 			Janitor = self.Props.Janitor or nil,
 			InitialValue = self.State[i],
+			CenteredText = true,
 			Priority = i,
+			Object = self.Props.Object,
 			OnValueChange = function(newValue)
 				if NumberField.IsFloat(newValue) then
 					newValue = math.floor(newValue)
@@ -113,16 +141,15 @@ function ColorField:Render(): GuiBase2d
 				newState[i] = newValue
 
 				self:SetState(newState)
-
-				print(table.unpack(newState))
 			end,
 		})
 
 		handler:Get().Parent = container
-		handler:Get().Size = UDim2.fromScale(0.33, 1)
 
 		self.Handlers[i] = handler
 	end
+
+	Unifier.relativeSize(Unifier.GetHandlerUIs(self.Handlers), container)
 
 	return container
 end
@@ -132,124 +159,3 @@ function ColorField:Get()
 end
 
 return ColorField
-
---[[
-return function(props: Props)
-	props.OnValueChange = props.OnStateChange ~= nil and props.OnStateChange or props.OnValueChang
-
-	local self = setmetatable({}, {})
-
-	self.ValueR = props.InitialValue.R or 255
-	self.ValueG = props.InitialValue.G or 255
-	self.ValueB = props.InitialValue.B or 255
-
-	-- Convert the unit to 255
-	if props.InitialValue then
-		self.ValueR *= 255
-		self.ValueG *= 255
-		self.ValueB *= 255
-	end
-
-	self.UpdateText = function(s)
-		s.Object.InputR.Text = tostring(math.floor(self.ValueR))
-		s.Object.InputG.Text = tostring(math.floor(self.ValueG))
-		s.Object.InputB.Text = tostring(math.floor(self.ValueB))
-	end
-
-	self.Object = New("Frame")({
-		Size = UDim2.new(0, 100, 0, 20),
-		BackgroundTransparency = 1,
-		Name = "NumberField",
-		LayoutOrder = props.Priority and props.Priority or 0,
-
-		[Children] = {
-			List = New("UIListLayout")({
-				FillDirection = Enum.FillDirection.Horizontal,
-				SortOrder = Enum.SortOrder.LayoutOrder,
-				Padding = UDim.new(0, 2.5),
-			}),
-
-			InputR = New("TextBox")({
-				Name = "InputR",
-				Size = UDim2.new(0.33, 0, 1, 0),
-				TextColor3 = Color3.fromHex("F15959"),
-				TextXAlignment = Enum.TextXAlignment.Center,
-				TextYAlignment = Enum.TextYAlignment.Center,
-				BackgroundColor3 = USE_COLOR and Color3.fromHex("DD3F3F") or DEF_COLOR,
-				LayoutOrder = 1,
-
-				[Fusion.OnEvent("FocusLost")] = function()
-					local newInput = tonumber(math.floor(self.Object.InputR.Text))
-					local onlyNums = doesOnlyContainNums(string.lower(tostring(newInput)))
-					newInput = math.clamp(newInput, 0, 255)
-
-					if onlyNums and tostring(newInput):len() <= 3 then
-						self.ValueR = newInput
-
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					else
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					end
-				end,
-			}),
-
-			InputG = New("TextBox")({
-				Name = "InputG",
-				Size = UDim2.new(0.33, 0, 1, 0),
-				TextColor3 = Color3.fromHex("6CE362"),
-				TextXAlignment = Enum.TextXAlignment.Center,
-				TextYAlignment = Enum.TextYAlignment.Center,
-				BackgroundColor3 = USE_COLOR and Color3.fromHex("43C538") or DEF_COLOR,
-				LayoutOrder = 2,
-
-				[Fusion.OnEvent("FocusLost")] = function()
-					local newInput = tonumber(math.floor(self.Object.InputG.Text))
-					local onlyNums = doesOnlyContainNums(string.lower(tostring(newInput)))
-					newInput = math.clamp(newInput, 0, 255)
-
-					if onlyNums and tostring(newInput):len() <= 3 then
-						self.ValueG = newInput
-
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					else
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					end
-				end,
-			}),
-
-			InputB = New("TextBox")({
-				Name = "InputB",
-				Size = UDim2.new(0.33, 0, 1, 0),
-				TextColor3 = Color3.fromHex("4E81EE"),
-				TextXAlignment = Enum.TextXAlignment.Center,
-				TextYAlignment = Enum.TextYAlignment.Center,
-				BackgroundColor3 = USE_COLOR and Color3.fromHex("385FC5") or DEF_COLOR,
-				LayoutOrder = 3,
-
-				[Fusion.OnEvent("FocusLost")] = function()
-					local newInput = tonumber(math.floor(self.Object.InputB.Text))
-					local onlyNums = doesOnlyContainNums(string.lower(tostring(newInput)))
-					newInput = math.clamp(newInput, 0, 255)
-
-					if onlyNums and tostring(newInput):len() <= 3 then
-						self.ValueB = newInput
-
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					else
-						props.OnValueChange(Color3.fromRGB(self.ValueR, self.ValueG, self.ValueB))
-						self:UpdateText()
-					end
-				end,
-			}),
-		},
-	})
-
-	self:UpdateText()
-
-	return self.Object
-end--]]
