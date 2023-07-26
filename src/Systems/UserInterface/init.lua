8935795df05faf8292fa2bf93675e66988ff44f7
James <86011536+jammees@users.noreply.github.com>
2023-07-26 16:15:54 +0200

    Use Theme, read size from config

--- src/Systems/UserInterface/init.lua
+++ src/Systems/UserInterface/init.lua
@@ -1,15 +1,18 @@
 local library = script.Parent.Parent.Library
 
-local ConfigSystem = require(script.Parent.Config)
+local ConfigSystem = require(script.Parent.Config).Get()
 
 local Janitor = require(library.Janitor).new()
 
 local ToolbarUI = require(script.Toolbar)
 local PropertyUI = require(script.Property)
+local MenuUI = require(script.Menu)
 
 local Fusion = require(library.Fusion)
 local Children = Fusion.Children
 local New = Fusion.New
+
+local Theme = require(script.Themes).GetTheme()
 
 local UserInterface = {}
 UserInterface.UI = nil
@@ -21,13 +24,13 @@ function UserInterface.Start(window: DockWidgetPluginGui)
 	UserInterface.UI = New("Frame")({
 		Parent = window,
 		Size = UDim2.fromScale(1, 1),
-		BackgroundColor3 = Color3.fromRGB(35, 68, 139),
+		BackgroundColor3 = Theme.WorkspaceBG,
 		ZIndex = -999999999,
 		Name = "Container",
 
 		[Children] = {
 			Workspace = New("Frame")({
-				Size = UDim2.new(1, 0, 1, -ConfigSystem.ui_TopbarOffset:get()),
+				Size = UDim2.new(1, -ConfigSystem.ui_PropertySize:get(), 1, -ConfigSystem.ui_TopbarOffset:get()),
 				Position = UDim2.fromOffset(0, 130),
 				BackgroundTransparency = 1,
 				Name = "Workspace",
@@ -48,12 +51,7 @@ function UserInterface.Start(window: DockWidgetPluginGui)
 				Name = "Editor",
 
 				[Children] = {
-					Menu = New("Frame")({
-						Name = "Menu",
-						Size = UDim2.new(1, 0, 0, 30),
-						BackgroundColor3 = Color3.fromRGB(22, 22, 22),
-					}),
-
+					Menu = MenuUI(),
 					Toolbar = ToolbarUI(),
 					Property = property,
 				},
